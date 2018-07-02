077c4dd06e9e6002c3c4bccd52c4c14eb6684651
Adams1991 <adams121@hotmail.co.uk>
2018-07-02 10:04:02 +0100

    setup of difference of squares exercise started

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,34 @@
+class Squares
+
+  def initialize(number)
+    @number = number
+  end
+
+
+  def self.square_of_sum()
+
+
+
+  end
+
+  # def self.sum_of_squares()
+  #
+  #
+  #
+  # end
+  #
+  # def self.difference()
+  #
+  # end
+
+
+
+end
+
+
+
+
+
+module BookKeeping
+  VERSION = 4
+end
