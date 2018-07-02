9a8f8f7e43073e0e14a7ea9e323d645868fdce0a
Adams1991 <adams121@hotmail.co.uk>
2018-07-02 10:54:25 +0100

    all tests passing

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,25 +1,31 @@
 class Squares
 
+  attr_reader(:number)
+
   def initialize(number)
     @number = number
   end
 
 
-  def self.square_of_sum()
-
-
-
+  def square_of_sum()
+    sum = 0
+    @number.downto(1) { |num| sum += num }
+    return sum ** 2
   end
 
-  # def self.sum_of_squares()
-  #
-  #
-  #
-  # end
-  #
-  # def self.difference()
-  #
-  # end
+
+
+  def sum_of_squares()
+    sum = 0
+    @number.downto(1) { |num| sum += (num**2)}
+    return sum
+  end
+
+
+
+  def difference()
+   return square_of_sum() - sum_of_squares()
+  end
 
 
 
